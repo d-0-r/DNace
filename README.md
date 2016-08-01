3f9bd580c0012dbeed666ea49cebca015fb8a706
Dor Rondel <dor.rondel@gmail.com>
2016-08-01 07:45:30 -0400

    some hardcoded progress, will need to refactor later

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# DNayyy
-A bioinformatics web app for FASTA file analysis using express, react, and chartJS
+# DNace
+
+Get it? Like the restriction enzyme :P
+
+A bioinformatics web app for FASTA file analysis using express, react, chartJS, stylus, trianglifyJS and MaterializeCSS.
+
+## Instructions
--- dev/js/index.js
+++ dev/js/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import ReactDOM from "react-dom";
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+  constructor() {
+    super();
+  }
+
   render() {
     return (
-      <div>
-        <h1><u> DNayyy </u></h1>
-      </div>
+      <main>
+        <h1> DNace </h1>
+        <h6> (Yes, the name is an intentional bad pun) </h6><br/>
+        <h4> Your basic DNA string sequencing made easy! </h4><br/>
+        <h5> Simply upload or paste in a FASTA formatted DNA string and </h5><br/>
+        <h5> get a quick analysis of the sequence including graphs and more! </h5><br/><br/>
+        <a className="waves-effect waves-light btn red upload-btn"><i className="material-icons right">file_upload</i>Upload</a>
+        <a className="waves-effect waves-light btn red paste-btn"><i className="material-icons right">content_paste</i>Paste</a>
+      </main>
     )
   }
 }
