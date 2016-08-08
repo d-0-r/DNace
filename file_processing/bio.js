8ad8af701bac4f9fb64b0f15536ab667da797ba1
Dor Rondel <dor.rondel@gmail.com>
2016-08-07 20:16:15 -0400

    finished bio module and added results.ejs

--- file_processing/bio.js
+++ file_processing/bio.js
@@ -28,6 +28,7 @@ class DnaAnalyzer {
       'GGA': 'G', 'GGG': 'G', 'GGC': 'G',
       'UGA': '-', 'UAA': '-', 'UAG': '-'
     };
+
     this.codon = {};
     this.aa = {};
 
@@ -40,16 +41,45 @@ class DnaAnalyzer {
     this.header = '';
     this.sequence = '';
     this.transcript = '';
+
+    this.adenineCount = 0;
+    this.thymineCount = 0;
+    this.guanineCount = 0;
+    this.cytosineCount = 0;
+    this.gcContent = 0;
+    this.totalBases = 0;
+    this.MB = 0;
+    this.totalAa = 0;
+  }
+
+  parseFASTA(dnaString, callback) {
+    let offset = 0;
+    for (let idx = 0; idx < dnaString.length; idx++) {
+      if (dnaString[0] === '>') {
+        if (dnaString[idx] !== '\n') {
+          this.header += dnaString[idx];
+          offset += 1;
+        } else { break; }
+      }
+    }
+
+    for (let idx = offset+1; idx < dnaString.length; idx++){
+      this.sequence += dnaString[idx];
+    }
+
+    if (callback) {
+      callback();
+    }
   }
 
   //reads fasta, calls parseFASTA, calls AnalyzeSequence
-  readFASTA(pathToFile, fileName) {
+  readFASTA(pathToFile, fileName, callback) {
     const lineReader = readline.createInterface({
       input: fs.createReadStream(__dirname + '/' + pathToFile + fileName) // relative path
     });
 
     lineReader.on('line', (line) => {
-      if (line[0] == '>') {
+      if (line[0] === '>') {
         this.header += line;
       } else {
         this.sequence += line;
@@ -58,12 +88,12 @@ class DnaAnalyzer {
 
     //when finished reading file, now we can parse input
     lineReader.on('close', () => {
-      console.log('done reading file');
+      console.log('done reading file\n');
       this.analyzeFASTA(this.sequence);
-      // console.log(this.transcript)
-      // console.log(this.nucleotides)
-      // console.log(this.codon)
-      // console.log(this.aa)
+      //this.dnaReport()
+      if (callback) {
+        callback();
+      }
     }.bind(this)); // switch context from interface to DnaAnalyzer
   }
 
@@ -78,7 +108,7 @@ class DnaAnalyzer {
     }
 
     /* Transcribe DNA to RNA */
-    this.transcript = seq.replace(/T/gi, "U");
+    this.transcript = seq.replace(/T/gi, 'U');
 
     /* Get codon usage & amino acid composition */
     for (let idx = 0; idx < seq.length; idx += 3) {
@@ -88,11 +118,54 @@ class DnaAnalyzer {
       }
     }
 
+    /* Get GC content, megabases and Total Bases */
+    // encapsulate regex match so it is evaluated first, then can use length method
+    this.adenineCount = this.nucleotides['A'];
+    this.thymineCount = this.nucleotides['T'];
+    this.cytosineCount = this.nucleotides['C'];
+    this.guanineCount = this.nucleotides['G'];
 
+    this.totalBases = this.guanineCount + this.cytosineCount + this.thymineCount + this.adenineCount;
+    this.MB = this.totalBases / 1000000.0;
+
+    this.gcContent = (((this.guanineCount + this.cytosineCount) / (this.totalBases)) * 100).toFixed(2);
+
+
+    /* Get total amino acid count */
+    let aaCount = 0;
+    for (let aa in this.aa) {
+      aaCount += this.aa[aa];
+    }
+    this.totalAa = aaCount;
+  }
+
+  // Won't be used, just to prettify things / testing
+  dnaReport() {
+    /* General Info */
+    console.log(this.header);
+    console.log('\n' + 'Total bases: ' + this.totalBases);
+    console.log('Mega Bases: ' + this.MB.toFixed(6));
+    console.log('GC content: ' + this.gcContent + '\n');
+
+    /* Base information */
+    for (let base in this.nucleotides) {
+      console.log(`${base} : ${(this.nucleotides[base] / this.totalBases * 100).toFixed(2)}%  ${this.nucleotides[base]}`);
+    }
+
+    /* amino acid composition */
+    console.log('\nAmino Acid Composition:\n----------------')
+    for (let aa in this.aa) {
+      console.log(`${aa} : ${(this.aa[aa] / this.totalAa * 100).toFixed(2)}%  ${this.aa[aa]}`);
+    }
+
+    /* codon usage */
+    console.log('\nCodon Usage:\n-----------------')
+    for (let codon in this.codon) {
+      console.log(`${codon} : ${this.codonTable[codon]}  ${(this.codon[codon] / this.totalAa * 100).toFixed(2)}%  ${this.codon[codon]}`);
+    }
   }
 }
 
-let d = new DnaAnalyzer();
-d.readFASTA('../uploadedFiles/', 'sequence.fasta');
-d.analyzeFASTA(d.sequence);
+// let d = new DnaAnalyzer();
+// d.readFASTA('../uploadedFiles/', 'sequence.fasta');
 module.exports.DnaAnalyzer = DnaAnalyzer;
